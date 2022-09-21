e7f7ab66dbe097df1fabc4f028a5d22c0eaee7e5
Omar Barragan <flaky_11@hotmail.com>
2022-09-21 17:28:14 -0500

    diseño de sidebar

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -1,50 +1,39 @@
 import React, { useState } from 'react';
-
-import SearchModal from '../components/ModalSearch';
-import Notifications from '../components/DropdownNotifications';
 import Help from '../components/DropdownHelp';
-import UserMenu from '../components/DropdownProfile';
+import UserMenu from '../partials/dashboard/DropdownProfile';
 
-function Header({
-  sidebarOpen,
-  setSidebarOpen
-}) {
-
-
+function Header({ sidebarOpen, setSidebarOpen }) {
   return (
-    <header className="sticky top-0 bg-white border-b border-slate-200 z-30">
-      <div className="px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16 -mb-px">
-
+    <header className='sticky top-0 bg-white border-b border-slate-200 z-30'>
+      <div className='px-4 sm:px-6 lg:px-8'>
+        <div className='flex items-center justify-between h-16 -mb-px'>
           {/* Header: Left side */}
-          <div className="flex">
-
+          <div className='flex'>
             {/* Hamburger button */}
             <button
-              className="text-slate-500 hover:text-slate-600 lg:hidden"
-              aria-controls="sidebar"
+              className='text-slate-500 hover:text-slate-600 lg:hidden'
+              aria-controls='sidebar'
               aria-expanded={sidebarOpen}
-              onClick={() => setSidebarOpen(!sidebarOpen)}
-            >
-              <span className="sr-only">Open sidebar</span>
-              <svg className="w-6 h-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <rect x="4" y="5" width="16" height="2" />
-                <rect x="4" y="11" width="16" height="2" />
-                <rect x="4" y="17" width="16" height="2" />
+              onClick={() => setSidebarOpen(!sidebarOpen)}>
+              <span className='sr-only'>Open sidebar</span>
+              <svg
+                className='w-6 h-6 fill-current'
+                viewBox='0 0 24 24'
+                xmlns='http://www.w3.org/2000/svg'>
+                <rect x='4' y='5' width='16' height='2' />
+                <rect x='4' y='11' width='16' height='2' />
+                <rect x='4' y='17' width='16' height='2' />
               </svg>
             </button>
-
           </div>
 
           {/* Header: Right side */}
-          <div className="flex items-center space-x-3">
-            <Help align="right" />
+          <div className='flex items-center space-x-3'>
+            <Help align='right' />
             {/*  Divider */}
-            <hr className="w-px h-6 bg-slate-200 mx-3" />
-            <UserMenu align="right" />
-
+            <hr className='w-px h-6 bg-slate-200 mx-3' />
+            <UserMenu align='right' />
           </div>
-
         </div>
       </div>
     </header>
